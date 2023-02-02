b5961783a75248c7519ccb28edb6c1149df23299
DaniloParavani <daniloparavani@yahoo.com.br>
2023-02-01 22:28:03 -0300

    Separando o Avatar em componente.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/87047957?v=4" />
+                    <Avatar src="https://avatars.githubusercontent.com/u/87047957?v=4" />
                     <div className={styles.authorInfo}>
                         <strong>Danilo Paravani</strong>
                         <span>Web Developer</span>
@@ -37,6 +39,12 @@ export function Post() {
                     <button type='submit'>Publicar</button>
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
         </article>
     )
 }
